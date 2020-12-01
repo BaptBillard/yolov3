61c78742244525be1ad3dddfec7f50b7d13c2b6b
simon.lebeaud@docaposte.fr <simon.lebeaud@docaposte.fr>
2020-12-01 21:50:59 +0100

    script pour mettre les images rgb avec les labels

---
+++ data/kitti_rep_format.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path, PosixPath
+import os
+
+if __name__ == '__main__':
+
+    img_size = (1242, 375)  # width, height: à vérifier
+
+    images_folder = 'training/images/'
+    output_labels_folder = "kitti/labels/"
+    images_pathes = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Cl.png")]
+
+    for image_path in images_pathes:
+        os.rename(image_path, output_labels_folder+image_path[-13:])
