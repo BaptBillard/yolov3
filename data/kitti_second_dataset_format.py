215e1422f815da631d6adf2e20840c76c97dae21
Bapt <baptiste.billard@insa-rouen.fr>
2020-12-12 23:18:15 +0100

    Script pour le deuxieme dataset fonctionnel

--- data/kitti_second_dataset_format.py
+++ data/kitti_second_dataset_format.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     img_size = (1242, 375)  # width, height: à vérifier
 
     images_folder = 'training/images/'
-    output_labels_folder = "kitti/images/"
+    output_folder = "second_kitti/images/"
     images_pathes_dp = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Dp.png")]
     images_pathes_of = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Of.png")]
     images_pathes_vl = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Vl.png")]
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     for i in range(len(images_pathes_dp)):
         tmp_merged_path = images_pathes_dp[i]
         tmp_merged_path = tmp_merged_path[:-7] + ".png"
-
+        tmp_merged_path = tmp_merged_path.rsplit('/', 2)
+        tmp_merged_path = output_folder + tmp_merged_path[2]
         tmp_img_dp = cv2.imread(images_pathes_dp[i], 0)
         tmp_img_of = cv2.imread(images_pathes_of[i], 0)
         tmp_img_vl = cv2.imread(images_pathes_vl[i], 0)
