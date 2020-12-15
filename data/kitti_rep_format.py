79a23ed79fdfa66815b6f184325311c6a4848838
Bapt <baptiste.billard@insa-rouen.fr>
2020-12-15 23:52:18 +0100

    Séparation train et val

--- data/kitti_rep_format.py
+++ data/kitti_rep_format.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path, PosixPath
 import os
+import random
 
 if __name__ == '__main__':
 
     img_size = (1242, 375)  # width, height: à vérifier
 
     images_folder = 'training/images/'
-    output_labels_folder = "kitti/images/"
+    output_train_folder = "kitti/images/train/"
+    output_val_folder = "kitti/images/val/"
     images_pathes = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Cl.png")]
 
+    # put 1/5 in val
+    random.shuffle(images_pathes)
+    images_pathes_val = []
+    for i in range(0,int(len(images_pathes)/5)):
+        images_pathes_val.append(images_pathes.pop())
+        tmp_path = output_val_folder + images_pathes_val[i][-13:]
+        tmp_path = tmp_path.rsplit("_", 1)[0] + ".png"
+        os.rename(images_pathes_val[i], tmp_path)
+
+
+    # put the 4/5 remaining in train
     for image_path in images_pathes:
-        os.rename(image_path, output_labels_folder+image_path[-13:])
-   
-    images_pathes = [str(PosixPath(path)) for path in Path(output_labels_folder).rglob("*_Cl.png")]
-    for image_path in images_pathes:
-        os.rename(image_path, image_path[0:19]+".png")
+        tmp_path = output_train_folder + image_path[-13:]
+        tmp_path = tmp_path.rsplit("_", 1)[0] + ".png"
+        os.rename(image_path, tmp_path)
+
--- data/kitti_second_dataset_format.py
+++ data/kitti_second_dataset_format.py
@@ -8,23 +8,43 @@ if __name__ == '__main__':
     img_size = (1242, 375)  # width, height: à vérifier
 
     images_folder = 'training/images/'
-    output_folder = "second_kitti/images/"
+    output_train_folder = "second_kitti/images/train/"
+    output_val_folder = "second_kitti/images/val/"
     images_pathes_dp = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Dp.png")]
     images_pathes_of = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Of.png")]
     images_pathes_vl = [str(PosixPath(path)) for path in Path(images_folder).rglob("*_Vl.png")]
 
-    # create merged images
-    for i in range(len(images_pathes_dp)):
-        tmp_merged_path = images_pathes_dp[i]
-        tmp_merged_path = tmp_merged_path[:-7] + ".png"
-        tmp_merged_path = tmp_merged_path.rsplit('/', 2)
-        tmp_merged_path = output_folder + tmp_merged_path[2]
-        tmp_img_dp = cv2.imread(images_pathes_dp[i], 0)
-        tmp_img_of = cv2.imread(images_pathes_of[i], 0)
-        tmp_img_vl = cv2.imread(images_pathes_vl[i], 0)
-
+    # put 1/5 in val (no shuffle here because of the 3 lists, not sure it changes anything anyway)
+    for i in range(0,int(len(images_pathes_dp)/5)):
+        tmp_merged_path = images_pathes_dp.pop()  # just in order to save the name
+        tmp_img_dp = cv2.imread(tmp_merged_path, 0)
+        tmp_img_of = cv2.imread(images_pathes_of.pop(), 0)
+        tmp_img_vl = cv2.imread(images_pathes_vl.pop(), 0)
+        
         # channel order can be changed if needed
         tmp_merged_image = cv2.merge((tmp_img_dp, tmp_img_of, tmp_img_vl))
 
         # save the newly created image
+        tmp_merged_path = tmp_merged_path[:-7] + ".png"
+        tmp_merged_path = tmp_merged_path.rsplit('/', 2)
+        tmp_merged_path = output_val_folder + tmp_merged_path[2]
+
+        cv2.imwrite(tmp_merged_path, tmp_merged_image)
+
+
+    # put the 4/5 remaining in the train
+    for image_path in images_pathes_dp:
+        tmp_merged_path = images_pathes_dp.pop()  # just in order to save the name
+        tmp_img_dp = cv2.imread(tmp_merged_path, 0)
+        tmp_img_of = cv2.imread(images_pathes_of.pop(), 0)
+        tmp_img_vl = cv2.imread(images_pathes_vl.pop(), 0)
+        
+        # channel order can be changed if needed
+        tmp_merged_image = cv2.merge((tmp_img_dp, tmp_img_of, tmp_img_vl))
+
+        # save the newly created image
+        tmp_merged_path = tmp_merged_path[:-7] + ".png"
+        tmp_merged_path = tmp_merged_path.rsplit('/', 2)
+        tmp_merged_path = output_train_folder + tmp_merged_path[2]
+
         cv2.imwrite(tmp_merged_path, tmp_merged_image)
